926b15033f6b761afa9f276ac0d5ff906b1d943c
[dyad] <mdrjavadi@gmail.com>
2026-01-27 11:21:37 +0100

    Reverted all changes back to version 6f913e6b0c03db86195d93aed0f79eb9139daabb

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import TopicDetail from "./pages/TopicDetail";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { Header } from "@/components/Header"; // Import the new Header component
+import { ThemeProvider as NextThemesProvider } from "next-themes"; // Changed import to NextThemesProvider
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class">
+    <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class"> {/* Used NextThemesProvider directly with attribute="class" */}
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <div className="relative min-h-screen flex flex-col">
-            <Header /> {/* Render the new Header component here */}
+            <div className="absolute top-4 right-4 z-50">
+              <ThemeToggle />
+            </div>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/:topicSlug" element={<TopicDetail />} />
