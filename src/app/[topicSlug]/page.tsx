f1f94321d71f720b9f6ab6254f45bfcc88919901
Arta <a.javadikouchaksarei@reply.de>
2026-01-27 13:43:01 +0100

    feat: add edge runtime support to page component

--- src/app/[topicSlug]/page.tsx
+++ src/app/[topicSlug]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = "edge";
+
 import React, { useState, useEffect, useMemo } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
