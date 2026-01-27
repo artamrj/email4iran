017867ab99b82a75c271d6a96a069d555a9f8434
[dyad] <mdrjavadi@gmail.com>
2026-01-27 01:53:33 +0100

    [dyad] Simplified theme provider setup by directly using `next-themes` `ThemeProvider` in `App.tsx` and removed the wrapper component. - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,33 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import TopicDetail from "./pages/TopicDetail"; // New import
-import { ThemeProvider } from "@/components/theme-provider"; // New import
-import { ThemeToggle } from "@/components/ThemeToggle"; // New import
+import TopicDetail from "./pages/TopicDetail";
+import { ThemeProvider as NextThemesProvider } from "next-themes"; // Changed import to NextThemesProvider
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> {/* Added ThemeProvider */}
+    <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class"> {/* Used NextThemesProvider directly with attribute="class" */}
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <div className="relative min-h-screen flex flex-col">
             <div className="absolute top-4 right-4 z-50">
-              <ThemeToggle /> {/* Added ThemeToggle */}
+              <ThemeToggle />
             </div>
             <Routes>
               <Route path="/" element={<Index />} />
-              <Route path="/:topicSlug" element={<TopicDetail />} /> {/* New route */}
+              <Route path="/:topicSlug" element={<TopicDetail />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </BrowserRouter>
       </TooltipProvider>
-    </ThemeProvider>
+    </NextThemesProvider>
   </QueryClientProvider>
 );
 
