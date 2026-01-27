da828910dae4efa89efd75419c32cfed103ca894
[dyad] <mdrjavadi@gmail.com>
2026-01-27 11:21:15 +0100

    [dyad] Unified header for navigation and theme toggle - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import TopicDetail from "./pages/TopicDetail";
-import { ThemeProvider as NextThemesProvider } from "next-themes"; // Changed import to NextThemesProvider
-import { ThemeToggle } from "@/components/ThemeToggle";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Header } from "@/components/Header"; // Import the new Header component
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class"> {/* Used NextThemesProvider directly with attribute="class" */}
+    <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme" attribute="class">
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
           <div className="relative min-h-screen flex flex-col">
-            <div className="absolute top-4 right-4 z-50">
-              <ThemeToggle />
-            </div>
+            <Header /> {/* Render the new Header component here */}
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/:topicSlug" element={<TopicDetail />} />
